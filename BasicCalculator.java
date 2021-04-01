0518b4bbe05cf96d72e9cfbca3b148fac2435429
ispi10 <79882056+ispi10@users.noreply.github.com>
2021-04-01 16:24:07 +0200

    calculators

--- BasicCalculator.java
+++ BasicCalculator.java
@@ -1,5 +1,30 @@
-package com.example.maven.first_project;
+package com.example.maven.calculator.classes;
+import com.example.maven.calculator_interfaces.BasicOperations;
 
 public class BasicCalculator {
+	
+	public double addition(double firstNumber, double secondNumber) {
+		return firstNumber + secondNumber;
+	}
+	public double subtraction(double firstNumber, double secondNumber) {
+		return firstNumber - secondNumber;
+	}
+
+	public double multiplication(double firstNumber, double secondNumber) {
+		return firstNumber * secondNumber;
+	}
+
+	public double division(double firstNumber, double secondNumber) {
+		double result = firstNumber / secondNumber;
+		if (Double.isInfinite(result)) {
+			System.out.println("Impossible");
+			return -0.125;
+		}
+		else {
+			return result;
+		}
+	}
+
+	
 
 }
